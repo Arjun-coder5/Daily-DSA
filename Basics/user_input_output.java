3a9ea951801b4541182b5a95b597cd63da639cae
Arjun-coder5 <arjunsorout526@gmail.com>
2025-05-15 13:22:34 +0530

    Added user_input_output.java file indide the folder

---
+++ Basics/user_input_output.java
@@ -0,0 +1,14 @@
+/*
+Problem: Multiplication of two numbers
+Link: (https://www.geeksforgeeks.org/problems/c-input-output2432/1)
+Idea: Simply multiply A and B and return the result
+Author: Arjun-coder5
+*/
+
+class Solution {
+    static int multiplication(int A, int B) {
+        // code here
+        int c = A * B;
+        return c;
+    }
+}
