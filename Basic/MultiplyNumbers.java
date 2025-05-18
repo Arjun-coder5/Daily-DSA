02363afbf619d83e576a6c2dd271e37793b6ccb3
Arjun-coder5 <arjunsorout526@gmail.com>
2025-05-18 12:23:32 +0530

    Addd Multiply numbers in java

---
+++ Basic/MultiplyNumbers.java
@@ -0,0 +1,13 @@
+
+// This is a simple Java program that defines a method to multiply two numbers.
+public class MultiplyNumbers {
+    public static int multiplication(int a, int b) {
+        return a * b;
+    }
+
+    // You can use this main method to test your function locally
+    public static void main(String[] args) {
+        System.out.println(multiplication(1, 2)); // Output: 2
+        System.out.println(multiplication(8, 7)); // Output: 56
+    }
+}
