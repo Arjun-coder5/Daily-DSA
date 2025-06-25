59cb23a8fafbb6e655b6751d0b71048ebda7c6a6
Arjun-coder5 <arjunsorout526@gmail.com>
2025-06-25 13:05:11 +0530

    Fix: Sync deleted README and added Bubble Sort

---
+++ Searching and Sorting/Sorting/Bubble_Sort.java
@@ -0,0 +1,31 @@
+
+import java.util.Arrays;
+
+public class Bubble_Sort{
+  static boolean swapped;
+
+  public static void main(String[] args) {
+    int[] arr = {1,2,3,4,5};
+    bubble(arr);
+    System.err.println(Arrays.toString(arr));
+  }
+ public  static void bubble(int[] arr){
+   int n = arr.length;
+   for(int i =0;i<n;i++){
+     swapped = false;
+    for(int j = 1;j<n-i;j++){
+      //Swap:
+      if(arr[j]<arr[j-1]){
+       int swap = arr[j];
+       arr[j] = arr[j-1];
+       arr[j-1] = swap;
+       swapped = true;
+
+      }
+    }
+    if(!swapped){
+      break;
+    }
+   }
+  }
+}
