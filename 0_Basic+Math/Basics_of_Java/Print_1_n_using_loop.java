9c369f46ebbe372b4d1632fe6209cbfb39d1f7da
Arjun-coder5 <arjunsorout526@gmail.com>
2025-06-28 22:14:01 +0530

    Added 02_Print_N_to_1.java in 0_Basics

---
+++ 0_Basic+Math/Basics_of_Java/Print_1_n_using_loop.java
@@ -0,0 +1,16 @@
+// Print numbers from 1 to N
+import java.util.Scanner;
+
+public class Print_1_n_using_loop {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter N: ");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            System.out.print(i + " ");
+        }
+
+        sc.close();
+    }
+}
