39127bf847c56ccdaa4be25862140a02b60a92e9
Arjun-coder5 <arjunsorout526@gmail.com>
2025-06-28 18:39:59 +0530

    Ques_1

---
+++ Strings/RemoveOutermostParentheses.java
@@ -0,0 +1,17 @@
+
+public class RemoveOutermostParentheses {
+    public String removeOuterParentheses(String s) {
+        StringBuilder result = new StringBuilder();
+        int count = 0;
+        for(char ch : s.toCharArray()) {
+            if(ch == '(') {
+                if(count > 0) result.append(ch);
+                count++;
+            } else {
+                count--;
+                if(count > 0) result.append(ch);
+            }
+        }
+        return result.toString();
+    }
+}
