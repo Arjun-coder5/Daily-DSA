81dad7ff0191b4e667beaf9debc926819c19aa28
Arjun-coder5 <arjunsorout526@gmail.com>
2025-07-29 08:36:57 +0530

    Ques_1

---
+++ 2_String/rotateString.java
@@ -0,0 +1,7 @@
+class rotateString {
+    public boolean rotateString(String s, String goal) {
+        if(s.length()!=goal.length())return false;
+        String comb = s+s;
+        return comb.contains(goal);
+    }
+}
